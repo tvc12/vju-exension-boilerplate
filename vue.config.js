209900efebd7ed551cf3b21d8c0f035d83db05fc
tvc12 - Vi Chi Thien <meomeocf98@gmail.com>
2020-06-23 10:24:52 +0700

    Add icon and update description

--- vue.config.js
+++ vue.config.js
@@ -9,8 +9,8 @@ module.exports = {
       new CopyPlugin({
         patterns: [
           {
-            from: path.resolve(__dirname, './extension/manifest.json'),
-            to: path.resolve(__dirname, './dist/manifest.json')
+            from: path.resolve(__dirname, './extension'),
+            to: path.resolve(__dirname, './dist')
           }
         ]
       })
